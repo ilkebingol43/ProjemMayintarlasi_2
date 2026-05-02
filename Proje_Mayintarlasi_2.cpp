2e4f090ff131d0515783319515158bba6ede6c8d
İlke Bingöl <ilkebingol43@gmail.com>
2026-05-02 17:03:37 +0300

    ekran açıldı ve png eklendi

--- Proje_Mayintarlasi_2.cpp
+++ Proje_Mayintarlasi_2.cpp
@@ -1,11 +1,45 @@
 ﻿// Proje_Mayintarlasi_2.cpp : Bu dosya 'main' işlevi içeriyor. Program yürütme orada başlayıp biter.
 //
+#include <SFML/Graphics.hpp> // Proje için gerekli olan kütüphane 
+#include <iostream>   // c++ main kütüphanesi
+#include <optional>   //
+#include <SFML/Window.hpp> // 
+using namespace std;
+using namespace sf;  // sf::Angle angle1 = sf::degrees(180); gibi kod karmaşalırnı engeller
+#include <chrono>  // c++ kendi zaman kütüpnahanesi
+#include <SFML/Window/Event.hpp> // olay tiplerini (KeyPressed, MouseMoved, vs.) kullanabilmek için.
 
-#include <iostream>
+
 
 int main()
 {
+    RenderWindow window;
+    window.create(VideoMode({ 800,640 }), "Mayin Tarlasi");
+
+
+    Texture texture;
+    if (!texture.loadFromFile("pngegg.png")) {
+        cerr << "Hata: pngegg.png yuklenemedi! Dosya adini ve konumunu kontrol edin." << endl;
+    }
+
+    Sprite sprite(texture);
     
+
+    while (window.isOpen()) {
+        while (const auto event = window.pollEvent()) {
+            if (event->is < Event::Closed>()) {
+                window.close();
+            }
+            window.clear(Color::White);
+
+            window.draw(sprite);
+
+            window.display();
+
+        }
+    }
+
+
 
     return 0 ; 
 }
