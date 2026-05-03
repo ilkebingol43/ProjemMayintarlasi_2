e077cf58051c1d95b5233f3a83460e622cac6257
İlke Bingöl <ilkebingol43@gmail.com>
2026-05-03 22:58:18 +0300

    Giris ve level ekranı Eklendi
    Detaylar : bomba sesi ve giriş ekranı muziği ekledim
    
    Yapilacaklar jpgleri ve ses dosyalarını tek bir dosya altında topladım isimler escape sequence ile çakışıyor bunları düzelticem

--- Proje_Mayintarlasi_2.cpp
+++ Proje_Mayintarlasi_2.cpp
@@ -8,36 +8,89 @@ using namespace std;
 using namespace sf;  // sf::Angle angle1 = sf::degrees(180); gibi kod karmaşalırnı engeller
 #include <chrono>  // c++ kendi zaman kütüpnahanesi
 #include <SFML/Window/Event.hpp> // olay tiplerini (KeyPressed, MouseMoved, vs.) kullanabilmek için.
-
+#include <SFML/Audio.hpp>
+#include <filesystem>
 
 
 int main()
 {
-    RenderWindow window;
-    window.create(VideoMode({ 800,640 }), "Mayin Tarlasi");
+    RenderWindow window; // ekran nesnesi
+    window.create(VideoMode({ 800,640 }), "Mayin Tarlasi"); // 800 e 640lık mayın Tarlasi isminde pencere 
 
+    // giriş ekranı
+    Texture firstScreen("Jpg\girisEkrani.jpg"); // Oyun için giriş ekranı
+    if (!firstScreen.loadFromFile("Jpg\girisEkrani.jpg")) { // png dosyası yüklendi  //GirisEkrani olmadı
+        cout << "GirisEkrani Yüklenemedi";  // hata                                  // c++ ta /G escape sequencetir
+     }
+    Sprite girisEkrani(firstScreen);
+    
 
-    Texture texture;
-    if (!texture.loadFromFile("pngegg.png")) {
-        cerr << "Hata: pngegg.png yuklenemedi! Dosya adini ve konumunu kontrol edin." << endl;
+    //Level Ekranı
+    Texture levelScreen("Jpg\Seviye.jpg");
+    if (!levelScreen.loadFromFile("Jpg\Seviye.jpg")) {
+        cout << "SeviyeEkrani Yüklenemedi" << endl;;
+        return -1;
+    }
+    Sprite seviyeEkrani(levelScreen);
+    
+     //Çalacak Müzüik
+    Music girisMuzigi("Ses\GirisMuzigi.mp3"); // oyun açıldığında çalacak olam müzik
+    if (!girisMuzigi.openFromFile("Jpg\GirisMuzigi.mp3")) { // eğer dosyaya ulaşamaz ise false döndürüp programı kapatır
+        cout << "Error" << endl;  // hata 
+        return -1;
     }
 
-    Sprite sprite(texture);
+    // bomba sesi
+    SoundBuffer bombaMuzigi("Ses\bombaSesi.wav"); // bombaSesi.wav buffere 
+    if (!bombaMuzigi.loadFromFile("Ses\bombaSesi.wav")) {  // eğer dosyaya ulaşamaz ise false döndürüp programı kapatır 
+        cout << "Error" << endl;
+    }
+    Sound bombaPatlama(bombaMuzigi);
+
+
+
+
+    girisMuzigi.setLooping(true);  // girisEkranını Müzik ayarları
+    girisMuzigi.setVolume(50);     // girisEkranın SesSeviyesi
+    girisMuzigi.setPitch(0.75f);   // girisEkranın sesFrekansı
+    girisMuzigi.play(); // müzik eklendi 
     
+
+
+
+    window.clear(); 
+    window.draw(girisEkrani); // program çalışmaya başladı an çıkıcak olan ekran
+    window.display();
 
     while (window.isOpen()) {
         while (const auto event = window.pollEvent()) {
-            if (event->is < Event::Closed>()) {
-                window.close();
+            if (event->is < Event::Closed>()) {  //
+                window.close();   // kapatma tuşuna basılınca pencereyi kapatır 
             }
-            window.clear(Color::White);
+            if (Keyboard::isKeyPressed(Keyboard::Key::Backspace)) { // Muziği durdurmak için backspace basmak
 
-            window.draw(sprite);
-
-            window.display();
-
+                girisMuzigi.stop(); 
+            }
+            if (const auto*  mouseClick = event->getIf < Event::MouseButtonReleased>()) {  // mouse sol tıkına basıldığında bomba sesi çıkar
+                if (mouseClick->button == Mouse::Button::Left) { 
+                    bombaPatlama.play();
+                }
+            }
+            if (Keyboard::isKeyPressed(Keyboard::Key::Enter)) {
+                window.clear();
+                window.draw(seviyeEkrani); //kullanıcı enter tuşuna basar ise level ekranı çıkıcak
+                window.display();
+            }
         }
+        
+          
+        
     }
+    
+
+
+   
+    
 
 
 
