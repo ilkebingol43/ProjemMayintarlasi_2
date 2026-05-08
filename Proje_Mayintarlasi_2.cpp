973f24c7cc99862ca0430b750c4dfd17c1047467
İlke Bingöl <ilkebingol43@gmail.com>
2026-05-08 18:40:15 +0300

    Ses artırma fonksiyonu düzeltildi
    kontorlcü durumu için enum class oluşturuldu yarın onu yapıcam

--- Proje_Mayintarlasi_2.cpp
+++ Proje_Mayintarlasi_2.cpp
@@ -1,4 +1,4 @@
-﻿#include <SFML/Graphics.hpp>
+﻿#include <SFML/Graphics.hpp>    
 #include <iostream>   // c++ main kütüphanesi
 #include <optional>   //
 #include <SFML/Window.hpp>;
@@ -8,8 +8,17 @@ using namespace sf;  // sf::Angle angle1 = sf::degrees(180); gibi kod karmaşal�
 #include <SFML/Window/Event.hpp> // olay tiplerini (KeyPressed, MouseMoved, vs.) kullanabilmek için.
 #include <SFML/Audio.hpp>
 #include <filesystem>
+#include <algorithm> // min ve max fonksiyonları için
 
 
+enum class gameState {  // kontrolcü çakışmasını engellemek için ve hangi ekrandayken hangi tuş ne işe yarayacak sorunun çözmek için enum class
+    girisEkrani,         
+    oyunEkrani,
+    cıkısEkrani,
+};
+
+
+void volumeChange(Keyboard::Key basilanTus, SoundSource& sesKaynagi);
 void ekranCiz(RenderWindow& window, Sprite jpg); //1. Prototipi referans olacak şekilde ayarlıyoruz
 int main()
 {
@@ -20,9 +29,21 @@ int main()
     Texture firstScreen("Jpg/jgirisEkrani.jpg"); // Oyun için giriş ekranı
     if (!firstScreen.loadFromFile("Jpg/jgirisEkrani.jpg")) { // png dosyası yüklendi  //GirisEkrani olmadı
         cout << "GirisEkrani Yüklenemedi";  // hata                                  // c++ ta /G escape sequencetir
+        return -1;
     }
     Sprite girisEkrani(firstScreen);
     girisEkrani.setScale(Vector2f(800.0f / 2816.0f, 640.0f / 1536.0f)); // 2820*1532 olarak aldığımız dosyayı // parametre olarak oran alır 800.f / 2816.f anlamı bu fotorafı yüzde 28 küçült
+    
+
+    Texture exit;
+    if (!exit.loadFromFile("Jpg/jCikisEkrani.jpg")) {
+        cout << "CıkısEkrani yüklenemedi" << endl ;
+        return - 1 ; // pencere açılmayacak
+    }
+    Sprite cikisEkrani(exit);
+    cikisEkrani.setScale(Vector2f(800.0f / 1600.0f, 640.0f / 872.0f));
+    
+    
 
     //Level Ekranı
     Texture levelScreen("Jpg/jSeviye.jpg");
@@ -31,7 +52,9 @@ int main()
         return -1;
     }
     Sprite seviyeEkrani(levelScreen);
+    seviyeEkrani.setScale(Vector2f(800.0f / 1375.0f, 640.0f / 768.0f)); // seviye Ekranı  pencereye göre ayarlandı // eğer f koyulmazsa 640 /768 yaklaşık 0.20 dir burdada 0 olarak kabul edilir ve siyah olur 
 
+    //--------------------------------------------------
     //Çalacak Müzüik
     Music girisMuzigi; // oyun açıldığında çalacak olam müzik
     if (!girisMuzigi.openFromFile("Ses/sGirisMuzigi.mp3")) { // eğer dosyaya ulaşamaz ise false döndürüp programı kapatır
@@ -41,13 +64,13 @@ int main()
 
 
     // bomba sesi
-    SoundBuffer bombaMuzigi("Ses/sbombaSesi.wav"); // bombaSesi.wav buffere 
-    if (!bombaMuzigi.loadFromFile("Ses/sbombaSesi.wav")) {  // eğer dosyaya ulaşamaz ise false döndürüp programı kapatır 
+    SoundBuffer tiklmamaMuzigi("Ses/stiklamaSesi.wav"); // bombaSesi.wav buffere 
+    if (!tiklmamaMuzigi.loadFromFile("Ses/stiklamaSesi.wav")) {  // eğer dosyaya ulaşamaz ise false döndürüp programı kapatır 
         cout << "Error" << endl;
     }
-    Sound bombaPatlama(bombaMuzigi);
+    Sound tiklamaSesi(tiklmamaMuzigi);
 
-    bombaPatlama.setVolume(0.75f); // tıklama sesi azaltma 
+    tiklamaSesi.setVolume(50.f); // tıklama sesi azaltma 
 
 
 
@@ -55,7 +78,7 @@ int main()
     girisMuzigi.setLooping(true);  // girisEkranını Müzik ayarları
     girisMuzigi.setVolume(50);     // girisEkranın SesSeviyesi
     girisMuzigi.setPitch(0.75f);   // girisEkranın sesFrekansı
-    girisMuzigi.play(); // müzik eklendi 
+    girisMuzigi.play(); // müzik eklendi  // 
 
 
     ekranCiz(window, girisEkrani); // eğer windowun yanına & koyarsak window nesnenin bellek adresi göndermiş oluruz
@@ -71,77 +94,36 @@ int main()
 
             }
             //-----------------------------------
-            if (const auto* stopVolume = event->getIf<Event::KeyPressed>()) { // Muziği durdurmak için backspace basmak
-                if (stopVolume->code == Keyboard::Key::F1) { // eğer f1 duracak
-                    girisMuzigi.pause(); //müzik durdu 
-
-                }
-
-            }
-            //------------------------------------------------
-            if (const auto* upVolume = event->getIf <Event::KeyPressed>()) {
-
-                if (upVolume->code == Keyboard::Key::F3) {
-
-                    if (girisMuzigi.getStatus() != SoundSource::Status::Playing) { // getStatus ile durum kontrolü SoundSource ilede oynayıp
-                        girisMuzigi.play();                                                            // oynayıp oynamadığı kontrol edlidli
-                    }
-                    float mevcutSes = girisMuzigi.getVolume();  //sesi artırmak için 
-
-                    girisMuzigi.setVolume(mevcutSes + 5.0f);
-                    if (mevcutSes < 100) {
-                        cout << "Ses Acildi" << girisMuzigi.getVolume(); // 
-                    }
-                    else {
-                        girisMuzigi.setVolume(100); // ses maximum 100 olarak kalır
-                        cout << "Ses Fullendi" << endl;
-                    }
-                }
-            }
-            // ----------------------
-            if (Keyboard::isKeyPressed(Keyboard::Key::F2)) {
-                if (girisMuzigi.getStatus() != SoundSource::Status::Playing) { // getStatus ile durum kontrolü SoundSource ilede oynayıp
-                    girisMuzigi.play();                                                            // oynayıp oynamadığı kontrol edlidli
-                }
-                float mevcutSes = girisMuzigi.getVolume(); // sesi azaltmak için
-
-                girisMuzigi.setVolume(mevcutSes - 5.0f);
-
-                if (mevcutSes < 100 && mevcutSes > 0) {
-                    cout << "Ses azaltildi " << girisMuzigi.getVolume();
-                }
-                else {
-                    girisMuzigi.setVolume(0);
-                    cout << "Ses kapatildi" << endl;
-
-                }
-
+            if (const auto* basilanTus = event->getIf <Event::KeyPressed>()) {
+                volumeChange(basilanTus->code, girisMuzigi); // basılacak tuşun kendisi gönderiyoruz
+                volumeChange(basilanTus->code, tiklamaSesi); //
             }
             // --------------------------------------------
-            if (const auto* mouseClick = event->getIf < Event::MouseButtonReleased>()) {  // mouse sol tıkına basıldığında bomba sesi çıkar
+            if (const auto* mouseClick = event->getIf < Event::MouseButtonPressed>()) {  // mouse sol tıkına basıldığında bomba sesi çıkar
                 if (mouseClick->button == Mouse::Button::Left) {
-                    bombaPatlama.play();
+                    tiklamaSesi.play();
+
                 }
             }
-            if (Keyboard::isKeyPressed(Keyboard::Key::Enter)) {
-                ekranCiz(window, seviyeEkrani);
+            //if (Keyboard::isKeyPressed(Keyboard::Key::Enter)) {
+            //    ekranCiz(window, seviyeEkrani);
+            //}
+            if (Keyboard::isKeyPressed(Keyboard::Key::Escape)) {
+                ekranCiz(window,cikisEkrani);
+      
             }
 
-        }
+        }// Sart kontrol
 
 
 
-    }
-
-
-
-
-
+    }// ekranı sürekli açık tutan döngü
 
 
 
     return 0;
-}
+}// end of main
+
 
 void ekranCiz(RenderWindow& window, Sprite jpg) { // nesneler referanslar ile fonksiyonlara bildirilir 
     // & operatörü bize direk nesnenin kendisi ile uğraşma imkanı sunar
@@ -149,4 +131,48 @@ void ekranCiz(RenderWindow& window, Sprite jpg) { // nesneler referanslar ile fo
     window.clear();
     window.draw(jpg);
     window.display();
-}
+}// end of function
+
+void volumeChange(Keyboard::Key basilanTus, SoundSource& sesKaynagi) {
+    
+    if (basilanTus == Keyboard::Key::F1) {
+        sesKaynagi.pause(); // şarkıyı o andan itibaren dondurur play komutu geldiğinde o andan itbaren başlaıtr 
+
+    }// ses kapatma şartı
+
+    if (basilanTus == Keyboard::Key::F3) {
+        if (sesKaynagi.getStatus() != SoundSource::Status::Playing) {
+            sesKaynagi.play();
+
+        }
+        float mevcutSes = sesKaynagi.getVolume();
+
+        if (mevcutSes < 100.0f) {
+            sesKaynagi.setVolume(min(mevcutSes + 5.0f, 100.0f));
+            cout << "Ses artirildi" << endl;
+        }
+        else {
+            cout << "Ses 100 den buyuk olamaz" << endl; // maximum 100 olabilir
+        }
+
+    } // ses artırma şartı  son
+
+    if (basilanTus == Keyboard::Key::F2)
+    {
+        if (sesKaynagi.getStatus() != SoundSource::Status::Playing) { // pause yada stopta ise 0 döndürecek 
+            sesKaynagi.play(); //
+
+        }
+        float mevcutSes = sesKaynagi.getVolume();                 
+
+        if (mevcutSes > 0.0f) {
+            sesKaynagi.setVolume(max(mevcutSes - 5.0f,0.0f));
+            cout << "Ses azaltildi" << endl;
+        }
+        else {
+            cout << "Ses 0 dan kucuk olamaz" << endl; // minumum 100 olabilir
+        } // ses azaltma son 
+
+
+    }
+}// end of function 
